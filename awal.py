a796eb4201001092ed8ecb2f248bd9b2b49a6ad2
Pandu Prasetyo <72228596+senyumpanda@users.noreply.github.com>
2021-02-09 05:21:50 +0700

    3 class python

--- awal.py
+++ awal.py
@@ -1,1 +1,8 @@
-print("Hello World")
+class Awal:
+    def __init__(self,nama):
+        self.nama = nama
+class Kedua(Awal):
+    def __str__(self):
+        print("Namaku {}".format(self.nama))
+class Ketiga(Kedua):
+    obj2 = Kedua("Joni")
