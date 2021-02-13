403c97397abb6e4e4aa153d0a894397b8e3232b2
Pandu Prasetyo <72228596+senyumpanda@users.noreply.github.com>
2021-02-13 10:09:28 +0700

    Delete awal.py

--- awal.py
+++
@@ -1,9 +0,0 @@
-class Awal:
-    def __init__(self,nama):
-        self.nama = nama
-class Kedua(Awal):
-    def __str__(self):
-        print("Hai {}".format(self.nama))
-class Ketiga(Kedua):
-    obj2 = Kedua("Jon")
-
